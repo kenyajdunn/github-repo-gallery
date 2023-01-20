669db3f05c0128376fa3246822c56a1b7d0b219e
kenya <kenyajdunn@gmail.com>
2023-01-20 15:25:58 -0600

    Fixed error is JS, profile shows up now

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const username = "kenyajdunn";
 
 const gitUserInfo = async function () {
     const userInfo = await fetch(
-      `https://api.github.com/users/${kenyajdunn}`);
+      `https://api.github.com/users/${username}`);
     const data = await userInfo.json();
     displayUserInfo(data);
   };
